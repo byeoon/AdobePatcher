974aec06d764c39bc5811579cd98dde68501e366
byron <47872200+byeoon@users.noreply.github.com>
2024-02-29 16:12:51 +0000

    casually no longer able to compile, lovely

--- main.cpp
+++ main.cpp
@@ -1,23 +1,41 @@
 #include <iostream>
 #include <curl/curl.h>
 #include <fstream>
-#pragma comment(lib, "winmm.lib")
 #include <string>
-#include <cpr/cpr.h>
 
 /*
 Test project
 */
+
+static size_t WriteCallback(void *contents, size_t size, size_t nmemb, void *userp)
+{
+    ((std::string*)userp)->append((char*)contents, size * nmemb);
+    return size * nmemb;
+}
+
 using namespace std;
-using namespace cpr;
-int main() {
+int main(void) {
    printf("\x1B[31m [Adobe] \033[0m\t\t");
    
    ofstream MyFile("filename.txt");
    MyFile << "eeeeee";
    MyFile.close();
    // PlaySound("", NULL, SND_FILENAME);
-   auto r = Get(Url{ "https://raw.githubusercontent.com/" });
-    cout << r.text << endl;
+
+  CURL *curl;
+  CURLcode res;
+  std::string readBuffer;
+
+  curl = curl_easy_init();
+  if(curl) {
+    curl_easy_setopt(curl, CURLOPT_URL, "http://www.google.com");
+    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
+    curl_easy_setopt(curl, CURLOPT_WRITEDATA, &readBuffer);
+    res = curl_easy_perform(curl);
+    curl_easy_cleanup(curl);
+
+    std::cout << readBuffer << std::endl;
+  }
+  
    return 0;
 }
