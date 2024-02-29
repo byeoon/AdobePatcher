378956a860d616d776a8fb041cbf9eef9b95a54f
byron <47872200+byeoon@users.noreply.github.com>
2024-02-29 17:04:25 +0000

    Hopefully added the host merge part

--- main.cpp
+++ main.cpp
@@ -2,40 +2,79 @@
 #include <curl/curl.h>
 #include <fstream>
 #include <string>
-
+#include <bits/stdc++.h>
+#include <sys/stat.h>
 /*
-Test project
+  Adobe Patching Software developed in C++.
+  (It probably won't work that well right now..)
 */
 
 static size_t WriteCallback(void *contents, size_t size, size_t nmemb, void *userp)
 {
-    ((std::string*)userp)->append((char*)contents, size * nmemb);
-    return size * nmemb;
+  ((std::string *)userp)->append((char *)contents, size * nmemb);
+  return size * nmemb;
 }
 
 using namespace std;
-int main(void) {
-   printf("\x1B[31m [Adobe] \033[0m\t\t");
-   
-   ofstream MyFile("filename.txt");
-   MyFile << "eeeeee";
-   MyFile.close();
-   // PlaySound("", NULL, SND_FILENAME);
+int main(void)
+{
+  printf("\x1B[31m [Adobe Patching Software] \033[0m\t\t");
+  printf("\n");
 
+  ofstream hostsCopy("hostsCopy.txt");
+  fstream file;
   CURL *curl;
   CURLcode res;
   std::string readBuffer;
+  const char *pathToHosts = "C:/Windows/System32/drivers/etc/hosts.txt";
+  struct stat sb;
 
   curl = curl_easy_init();
-  if(curl) {
-    curl_easy_setopt(curl, CURLOPT_URL, "http://www.google.com");
+  if (curl)
+  {
+    curl_easy_setopt(curl, CURLOPT_URL, "https://raw.githubusercontent.com/ignaciocastro/adobe-is-dumb/main/127.txt");
     curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
     curl_easy_setopt(curl, CURLOPT_WRITEDATA, &readBuffer);
     res = curl_easy_perform(curl);
     curl_easy_cleanup(curl);
 
-    std::cout << readBuffer << std::endl;
+    hostsCopy << readBuffer << std::endl;
+    printf("\x1B[32m [+] Completed adding urls to the copied hosts file. \033[0m\t\t");
+    printf("\n");
   }
-  
-   return 0;
+  hostsCopy.close();
+
+  printf("\x1B[31m [~] Merging copied and original hosts files together...  \033[0m\t\t");
+  printf("\n");
+  // PlaySound("", NULL, SND_FILENAME);
+  if (stat(pathToHosts, &sb) == 0)
+  {
+    cout << "Path to hosts file exists.";
+    ifstream ifile("hostsCopy.txt", ios::in);
+    ofstream ofile("C:/Windows/System32/drivers/etc/hosts.txt", ios::out | ios::app);
+
+    if (!ifile.is_open())
+    {
+      cout << "file not found";
+    }
+    else
+    {
+      ofile << ifile.rdbuf();
+    }
+    string word;
+
+    file.open("C:/Windows/System32/drivers/etc/hosts.txt");
+    while (file >> word)
+    {
+      printf("\n");
+      printf("\x1B[32m [+] Appending new hosts \033[0m\t\t");
+    }
+  }
+  else
+  {
+    cout << "The hosts path does not exist.";
+    printf("\n");
+  }
+
+  return 0;
 }
